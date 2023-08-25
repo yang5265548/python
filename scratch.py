77c7d748580b680e4ad5243642b8189db060c37b
yangyang <yang.yang@metropolia.fi>
2023-08-25 15:59:55 +0800

    Initial Commit

---
+++ scratch.py
@@ -0,0 +1,8 @@
+
+pi=3.141592656
+formatPI="value of PI:{:.5f}".format(pi)
+print(formatPI)
+radius=float(input("Enter the radius: "))
+areaCircle=pi * radius*radius
+print("Are of circle areaCircle{:.2f}".format(areaCircle))
+print(f"Are of circle areaCircle {areaCircle:.4f}")
